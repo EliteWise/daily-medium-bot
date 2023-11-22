43c08c0efce43a4d4edb972c409a3bd546d6eca6
Florent <eliteman.fm@gmail.com>
2023-11-22 20:02:28 +0100

    Restore cronTab expression

--- scheduler.go
+++ scheduler.go
@@ -8,18 +8,18 @@ import (
 
 func sendArticle() {
 	// Create new scheduler
-	c := cron.New(cron.WithSeconds()) // To use seconds, add: `cron.WithSeconds()` inside New() func
+	c := cron.New() // To use seconds, add: `cron.WithSeconds()` inside New() func
 
 	fmt.Printf("Scheduler is starting...")
 
-	/*var setupData SetupData
+	var setupData SetupData
 	sendingTime := deserializeData(CONFIG_SOURCE, setupData.HourToSend)
-	cronExpression := fmt.Sprintf("0 %d * * *", sendingTime)*/
+	cronExpression := fmt.Sprintf("0 %d * * *", sendingTime)
 
 	emptyID := ""
 
 	// Add the task to be executed every day
-	c.AddFunc("*/15 * * * * *", func() { // To test every x seconds, use: "*/15 * * * * *"
+	c.AddFunc(cronExpression, func() { // To test every x seconds, use: "*/15 * * * * *"
 		var setupData SetupData
 		deserializeData(CONFIG_SOURCE, &setupData)
 		if setupData.Mode == "channel_mode" {
